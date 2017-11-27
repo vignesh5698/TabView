0394f5a2a8a81a82bd7d8159c0b21756b8f71bd4
vignesh5698 <vigneshit13@gmail.com>
2017-11-28 01:13:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/root/test/Onefrag.java
@@ -0,0 +1,27 @@
+package com.example.root.test;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by root on 11/28/17.
+ */
+
+public class Onefrag extends Fragment {
+    public Onefrag(){}
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.onefrag, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/root/test/Twofrag.java
@@ -0,0 +1,27 @@
+package com.example.root.test;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by root on 11/28/17.
+ */
+
+public class Twofrag extends Fragment {
+
+    public Twofrag(){}
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.twofrag,container,false);
+            }
+}
